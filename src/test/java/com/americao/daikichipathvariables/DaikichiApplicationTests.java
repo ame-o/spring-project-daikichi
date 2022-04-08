d7aff551f05fc9b4089e3658880ee8e3f834347f
America Ocampo <americavocampo@gmail.com>
2022-04-08 07:12:42 -0700

    first commit

---
+++ src/test/java/com/americao/daikichipathvariables/DaikichiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.americao.daikichipathvariables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
